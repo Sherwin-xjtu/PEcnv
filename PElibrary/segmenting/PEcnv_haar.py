fb4c2cca903b73fe0036b82fb1c1a36ac1251133
Sherwin-xjtu <52821375+Sherwin-xjtu@users.noreply.github.com>
2026-06-17 18:12:59 +0800

    Refactor FDRThres function for clarity and functionality
    We thank BioTester (https://github.com/lianlianlian6/BioTester) and its contributor for their feedback and suggestions during the development and improvement of this project.

--- PElibrary/segmenting/PEcnv_haar.py
+++ PElibrary/segmenting/PEcnv_haar.py
@@ -128,7 +128,7 @@ def haarSeg(I, breaksFdrQ,
             'size': segEd - segSt,
             'mean': segs[segSt]}
 
-
+"""
 def FDRThres(x, q, stdev):
     M = len(x)
     if M < 2:
@@ -146,7 +146,29 @@ def FDRThres(x, q, stdev):
                       p[0], m[0], q)
         T = x_sorted[0] + 1e-16
     return T
+"""
 
+
+def FDRThres(x, q, stdev):
+    """"""False discovery rate (FDR) threshold.""""""
+    M = len(x)
+    if M < 2:
+        return 0
+
+    m = np.arange(1, M + 1) / M
+    x_sorted = np.sort(np.abs(x))[::-1]
+    p = 2 * (1 - stats.norm.cdf(x_sorted, loc=0, scale=stdev))  # like R ""pnorm""
+    # Get the largest index for which p <= m*q
+    indices = np.nonzero(p <= m * q)[0]
+    if len(indices):
+        T = x_sorted[indices[-1]]
+    else:
+        logging.debug(
+            ""No passing p-values: min p=%.4g, min m=%.4g, q=%s"", p[0], m[0], q
+        )
+        T = x_sorted[0] + 1e-16  # ~= 2^-52, like MATLAB ""eps""
+    return T
+    
 
 def SegmentByPeaks(data, peaks, weights=None):
     segs = np.zeros_like(data)
