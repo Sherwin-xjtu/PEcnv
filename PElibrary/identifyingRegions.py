ef3dca7c61841f20b14b6cf434538c7b1466c0f5
Sherwin-xjtu <52821375+Sherwin-xjtu@users.noreply.github.com>
2026-06-17 18:20:21 +0800

    Update
    We thank BioTester (https://github.com/lianlianlian6/BioTester) and its contributor for their feedback and suggestions during the development and improvement of this project.

--- PElibrary/identifyingRegions.py
+++ PElibrary/identifyingRegions.py
@@ -141,7 +141,7 @@ def haarSeg(I, breaksFdrQ,
             'size': segEd - segSt,
             'mean': segs[segSt]}
 
-
+"""
 def FDRThres(x, q, stdev):
     """False discovery rate (FDR) threshold."""
     M = len(x)
@@ -159,6 +159,28 @@ def FDRThres(x, q, stdev):
         logging.debug("No passing p-values: min p=%.4g, min m=%.4g, q=%s",
                       p[0], m[0], q)
         T = x_sorted[0] + 1e-16  # ~= 2^-52, like MATLAB "eps"
+    return T
+"""
+
+
+def FDRThres(x, q, stdev):
+    """"""False discovery rate (FDR) threshold.""""""
+    M = len(x)
+    if M < 2:
+        return 0
+
+    m = np.arange(1, M + 1) / M
+    x_sorted = np.sort(np.abs(x))[::-1]
+    p = 2 * (1 - stats.norm.cdf(x_sorted, loc=0, scale=stdev))  # like R ""pnorm""
+    # Get the largest index for which p <= m*q
+    indices = np.nonzero(p <= m * q)[0]
+    if len(indices):
+        T = x_sorted[indices[-1]]
+    else:
+        logging.debug(
+            ""No passing p-values: min p=%.4g, min m=%.4g, q=%s"", p[0], m[0], q
+        )
+        T = x_sorted[0] + 1e-16  # ~= 2^-52, like MATLAB ""eps""
     return T
 
 
